9b725d8792e2c5425254665536922a7e2787098d
Sean Bain <artemis45566@gmail.com>
2023-09-12 14:49:22 +0100

    Changed controls.

--- script.js
+++ script.js
@@ -11,7 +11,12 @@ let selectedColor = 'black';
 let backgroundImage = new Image();
 
 
+let isDrawing = false;
+
 let pixels = [];
+
+
+let gridDisplay = true;
 
 
 // image upload
@@ -43,6 +48,16 @@ function handleImageUpload(event) {
 
 
 
+// display grid
+
+let gridDisplayButton = document.getElementById('gridDisplayButton');
+gridDisplayButton.addEventListener('click', (event) => {
+
+
+    drawGrid();
+})
+
+
 // color picker
 
 let colorPicker = document.getElementById('colorPicker');
@@ -58,8 +73,14 @@ let gridSizePicker = document.getElementById('gridSizePicker');
 gridSizePicker.addEventListener('change', (event) => {
 
     // gridSize = event.target.value;
+    
 })
 
+
+// export button
+
+let exportButton = document.getElementById('exportButton');
+exportButton.addEventListener('click', exportCanvasImage);
 
 
 // GRID
@@ -67,21 +88,45 @@ gridSizePicker.addEventListener('change', (event) => {
 
 const drawGrid =  () => {
 
-    // Draw vertical lines
-    for (var x = 0; x <= canvasWidth; x += gridSize) {
-        ctx.beginPath();
-        ctx.moveTo(x, 0);
-        ctx.lineTo(x, canvasHeight);
-        ctx.stroke();
+
+    if (gridDisplay) {
+
+        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+
+        ctx.globalAlpha = 0.5;
+        ctx.drawImage(backgroundImage, 0, 0, canvas.width, canvas.height);
+        ctx.globalAlpha = 1;
+
+        pixels.forEach((pixel) => {
+            ctx.fillStyle = pixel.color;
+            ctx.fillRect(pixel.x, pixel.y, gridSize, gridSize);
+        })
+
+        gridDisplay = false;
+
+    } else {
+
+            // Draw vertical lines
+        for (var x = 0; x <= canvasWidth; x += gridSize) {
+            ctx.beginPath();
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, canvasHeight);
+            ctx.stroke();
+        }
+
+        // Draw horizontal lines
+        for(var y = 0; y <= canvasHeight; y += gridSize) {
+            ctx.beginPath();
+            ctx.moveTo(0, y);
+            ctx.lineTo(canvasWidth, y);
+            ctx.stroke();
+        }
+
+
+        gridDisplay = true;
     }
 
-    // Draw horizontal lines
-    for(var y = 0; y <= canvasHeight; y += gridSize) {
-        ctx.beginPath();
-        ctx.moveTo(0, y);
-        ctx.lineTo(canvasWidth, y);
-        ctx.stroke();
-    }
+    
 
 
 }
@@ -94,14 +139,73 @@ const drawGrid =  () => {
 
 // event listeners
 
+canvas.addEventListener('mouseup', () => {
+    
+    isDrawing = false;
+    pixels.forEach((pixel) => {
+
+         // Draw the pixel
+        ctx.fillStyle = pixel.color;
+        ctx.fillRect(pixel.x, pixel.y, gridSize, gridSize);
+    })
+
+    
+})
+
+
+
+canvas.addEventListener('mousemove', (event) => {
+
+    
+
+    if(isDrawing===true) {
+
+        let rect = canvas.getBoundingClientRect();
+        let x = event.clientX - rect.left;
+        let y = event.clientY - rect.top;
+
+        // snap coordinates to grid
+        let gridX = 0;
+        let gridY = 0;
+
+        for (let i = 0; i < canvasWidth; i+=gridSize) {
+            if(x >= i && x <= (i+gridSize)) {
+                gridX = i;
+            }
+        }
+
+        for (let i = 0; i < canvasHeight; i+=gridSize) {
+            if(y >= i && y <= (i+gridSize)) {
+                gridY = i;
+            }
+        }
+        
+
+
+        // Add pixel to global array
+
+        pixels.push({
+            x: gridX,
+            y: gridY,
+            color: selectedColor
+        })
+
+    }
+})
+
+
+
 // DRAW
 
-canvas.addEventListener('click', (event) => {
+canvas.addEventListener('mousedown', (event) => {
 
     if (event.altKey) {
 
         return
     }
+
+    isDrawing = true;
+    
 
     let rect = canvas.getBoundingClientRect();
     let x = event.clientX - rect.left;
@@ -133,12 +237,12 @@ canvas.addEventListener('click', (event) => {
         color: selectedColor
     })
 
-
+    
 
     // Draw the pixel
     ctx.fillStyle = selectedColor;
     ctx.fillRect(gridX, gridY, gridSize, gridSize);
-
+   
 
 });
 
@@ -150,7 +254,7 @@ canvas.addEventListener('click', (event) => {
 
     if (event.altKey) {
 
-        console.log(pixels);
+        
         pixels.pop();
         ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
@@ -168,6 +272,35 @@ canvas.addEventListener('click', (event) => {
 
 
 })
+
+
+
+
+// Export image
+
+function exportCanvasImage() {
+
+
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    pixels.forEach((pixel) => {
+        ctx.fillStyle = pixel.color;
+        ctx.fillRect(pixel.x, pixel.y, gridSize, gridSize);
+    })
+
+    // Create a temporary link element
+    const link = document.createElement('a');
+    link.setAttribute('download', 'pixie.png');
+
+    // convert the canvas image to a data URL
+    const dataURL = canvas.toDataURL('image/png');
+
+    // set the data URL as the href attribute of the link
+    link.href = dataURL;
+
+    // Simulate a click on the link to trigger the download
+    link.click();
+}
+
 
 
 
